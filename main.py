614b112b832b30fbb29c717e015af5b96598bd5c
hephaestou <rcpdoonan@gmail.com>
2026-05-19 18:32:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,17 @@ os.environ['KIVY_WINDOW'] = 'sdl2'
 
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.graphics import Color, Rectangle
+from kivy.uix.boxlayout import BoxLayout
 
 class TestApp(App):
     def build(self):
-        return Label(text='Darvidog is working!')
+        layout = BoxLayout()
+        with layout.canvas.before:
+            Color(1, 0, 0, 1)
+            self.rect = Rectangle(size=layout.size, pos=layout.pos)
+        layout.add_widget(Label(text='DARVIDOG WORKING!', font_size='30sp', color=(1,1,1,1)))
+        return layout
 
 if __name__ == '__main__':
     TestApp().run()
